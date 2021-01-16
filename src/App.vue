36674b26e32ccc4f554e1937c117a20d7f12a4f4
kihwankim <clack2933@gmail.com>
2021-01-16 22:09:39 +0900

    feat(todo-list): todo list페이지 뼈대 구현

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,37 @@
 <template>
-  <div id="app" class="container">
-    <h1 class="text-center">Todo App</h1>
-    <input type="text" class="w-100 p-2" placeholder="Type todo"/>
-    <div>
-      <input type="checkbox"/>
-      <span class="ml-3">Buy a car</span>
+    <div id="app" class="container">
+        <h1 class="text-center">Todo App</h1>
+        <input type="text" class="w-100 p-2" placeholder="Type todo" @keyup.enter="addTodo"/>
+        <hr/>
+        <Todo v-for="todo in todos" :key="todo.id" :todo="todo"/>
     </div>
-  </div>
 </template>
 
 <script>
-export default {
-  name: 'App',
-}
+    import Todo from '@/components/Todo.vue'
+    export default {
+        components: {
+            Todo
+        },
+        data() {
+            return {
+                increamentId: 0,
+                todos: [
+                ]
+            }
+        },
+        methods: {
+            addTodo(event) {
+                this.todos.push({
+                  id: this.increamentId,
+                  text: event.target.value,
+                  checked: false
+                });
+                this.increamentId += 1;
+                event.target.value = '';
+            }
+        } // keyup.enter : enter button이 클릭 되면 일어나는 action
+    }
 </script>
 
-<style>
-</style>
+<style></style>
