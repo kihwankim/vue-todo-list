05b2d20744438bcd1c5ed45d1d1d68fa58264b52
kihwankim <clack2933@gmail.com>
2021-01-16 21:57:12 +0900

    feat: init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <h1 class="text-center">Todo App</h1>
+    <input type="text" class="w-100 p-2" placeholder="Type todo"/>
+    <div>
+      <input type="checkbox"/>
+      <span class="ml-3">Buy a car</span>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
